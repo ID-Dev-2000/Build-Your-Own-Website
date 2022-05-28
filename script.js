7531161e39538980fcb90c9c9d68baf4948904f8
ID <izaakduncan@outlook.com>
2022-05-27 17:42:44 -0700

    added instructions

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 // Elements to input user data
 let inputDiv = document.getElementById('inputDiv')
-let inputWebsiteName = document.getElementById('inputWebsiteName')
 let inputBodyText = document.getElementById('inputBodyText')
 let submitDataButton = document.getElementById('submitDataButton')
+let htmlInstructions = document.getElementById('htmlInstructions')
 
 // Elements to display data from user input
 let userContentDiv = document.getElementById('userContentDiv')
-let userWebsiteName = document.getElementById('userWebsiteName')
 let userBodyText = document.getElementById('userBodyText')
 let resetPageButton = document.getElementById('resetPageButton')
 
-// Event listeners to display new page from user data, reset page to default, 
-inputBodyText.addEventListener("keydown", event => {
-    if (event.key === "Enter") {
-        displayNewPage()
-    }
-})
+// Event listeners to display new page from user data, reset page to default
 submitDataButton.addEventListener("click", displayNewPage)
 resetPageButton.addEventListener("click", resetPageToDefault)
 
@@ -24,21 +18,22 @@ userContentDiv.style.display = "none"
 
 // All functions below
 function updatePageWithUserData() {
-    userWebsiteName.innerHTML = inputWebsiteName.value
     userBodyText.innerHTML = inputBodyText.value
 }
 
 function resetPageToDefault() {
     inputDiv.style.display = ""
+    htmlInstructions.style.display = ""
     userContentDiv.style.display = "none"
 }
 
 function displayNewPage() {
-    if (inputWebsiteName.value == '' || inputBodyText == '') {
+    if (inputBodyText.value == '') {
         resetPageToDefault()
     } else {
         updatePageWithUserData()
         inputDiv.style.display = "none"
+        htmlInstructions.style.display = "none"
         userContentDiv.style.display = ""
     }
 }
